b91734270cb3d11c2dc608fb75711b2655bb7c9b
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2024-09-06 16:06:14 -0700

    Inicio de proyecto

---
+++ src/main/java/com/mycompany/Main/GestionInventario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.Main;
+
+/**
+ *
+ * @author Ramosz
+ */
+public class GestionInventario implements IInventariar {
+
+    @Override
+    public void agregarNuevoProducto(Producto nuevoProducto) {
+
+    }
+
+    @Override
+    public void actualizarProducto(String nombreProducto) {
+
+    }
+
+    @Override
+    public void eliminarProducto(String nombreProducto) {
+
+    }
+
+}
---
+++ src/main/java/com/mycompany/Main/Producto.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Main;
+
+/**
+ *
+ * @author Ramosz
+ */
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private String precio;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, String descripcion, String precio, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{");
+        sb.append("nombre=").append(nombre);
+        sb.append(", descripcion=").append(descripcion);
+        sb.append(", precio=").append(precio);
+        sb.append(", cantidad=").append(cantidad);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
